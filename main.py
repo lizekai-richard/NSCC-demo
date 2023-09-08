74226041364b79200b62a3c15d995cb76a952d81
Li Zekai <e0817846@asp2a-login-nus01.head.cm.asp2a.nscc.sg>
2023-09-08 12:28:06 +0800

    Modify the code

--- main.py
+++ main.py
@@ -8,9 +8,18 @@ def load_model(model_path):
     return tokenizer, model
 
 
+
+
+
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument("--model_path", type=str, default="/path/to/model")
     args = parser.parse_args()
 
-    load_model(args.model_path)
+    tokenizer, model = load_model(args.model_path)
+
+    input = "A step by step recipe to make bolognese pasta: "
+    tokenized_input = tokenizer(input, return_tensors='pt').input_ids
+    output_ids = model.generate(tokenized_input)
+    output = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
+    print(output)
